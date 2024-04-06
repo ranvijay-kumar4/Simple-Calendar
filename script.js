b2d5c2a27712c1719dec0dc852240c7ac9c9abb8
RANVIJAY KUMAR <116416700+ranvijay-kumar4@users.noreply.github.com>
2024-04-07 02:12:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const date = new Date()
 const weekDay = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
-document.getElementById('date').innerHTML = date.getDate()
-document.getElementById('day').innerHTML = weekDay[date.getDay()]
-document.getElementById('month').innerHTML = months[date.getMonth()]
-document.getElementById('year').innerHTML = date.getFullYear()
-
+document.getElementById('date').innerHTML = date.getDate();
+document.getElementById('day').innerHTML = weekDay[date.getDay()];
+document.getElementById('month').innerHTML = months[date.getMonth()];
+document.getElementById('year').innerHTML = date.getFullYear();
