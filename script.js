43b8ccf2e4bdb631f473e02f256778a5e03955e7
RANVIJAY KUMAR <116416700+ranvijay-kumar4@users.noreply.github.com>
2024-04-07 02:08:31 +0530

    Simple Calendar

---
+++ script.js
@@ -0,0 +1,9 @@
+const date = new Date()
+const weekDay = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+document.getElementById('date').innerHTML = date.getDate()
+document.getElementById('day').innerHTML = weekDay[date.getDay()]
+document.getElementById('month').innerHTML = months[date.getMonth()]
+document.getElementById('year').innerHTML = date.getFullYear()
+
